322ec4667f9080d5bba8ec9e1e3e69acd14c0521
Simo Rudra <simo.rudra@gmail.com>
2026-05-28 05:39:54 +0000

    Publish library rudra-core version 1.0.54

--- libs/rudra-core/src/index.ts
+++ libs/rudra-core/src/index.ts
@@ -1,11 +1,14 @@
 import './index.css';
 
-export type { ButtonProps } from './components/Button';
-export { default as Button } from './components/Button';
-export type { FlipTileProps } from './components/FlipTile';
-export { default as FlipTile } from './components/FlipTile';
-export type { RecordItem, RecordListProps } from './components/RecordList';
-export { default as RecordList } from './components/RecordList';
-export { default as ReactionGame } from './components/Test';
+export type { BadgeProps } from './components/Badge';
+export { default as Badge } from './components/Badge';
+export type { HeadingProps } from './components/Heading';
+export { default as Heading } from './components/Heading';
+export type { ImageProps } from './components/Image';
+export { default as Image } from './components/Image';
+export type { TypographyProps } from './components/Typography';
+export { default as Typography } from './components/Typography';
+export type { VideoProps } from './components/Video';
+export { default as Video } from './components/Video';
 export type { FormatterProps } from './utils/formatter';
 export { default as formatter } from './utils/formatter';
