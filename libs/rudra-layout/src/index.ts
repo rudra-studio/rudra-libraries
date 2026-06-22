10299caccd01bb47e389f9ad341866a2bed11fea
Simo Rudra <simo.rudra@gmail.com>
2026-06-22 15:07:06 +0000

    Publish library rudra-layout version 1.0.5

--- libs/rudra-layout/src/index.ts
+++ libs/rudra-layout/src/index.ts
@@ -2,8 +2,11 @@ import './index.css';
 
 export type { GridProps } from './components/Grid';
 export { default as Grid } from './components/Grid';
-export { RepeaterContext } from './components/Repeater';
 export type { RepeaterProps } from './components/Repeater';
 export { default as Repeater } from './components/Repeater';
+export type { RepeaterTableProps } from './components/RepeaterTable';
+export { default as RepeaterTable } from './components/RepeaterTable';
 export type { StackProps } from './components/Stack';
 export { default as Stack } from './components/Stack';
+export type { DataTableProps } from './components/Table';
+export { default as DataTable } from './components/Table';
