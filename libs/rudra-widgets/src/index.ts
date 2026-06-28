c5a82fcb0532e1ea6bda7ac9da457d032fc888d6
Simo Rudra <simo.rudra@gmail.com>
2026-06-28 01:28:56 +0000

    Publish library rudra-widgets version 1.0.1

---
+++ libs/rudra-widgets/src/index.ts
@@ -0,0 +1,16 @@
+import './index.css';
+
+export type { RudraCarouselProps } from './components/Carousel';
+export { default as RudraCarousel } from './components/Carousel';
+export type { AccordionItem, DataAccordionProps } from './components/DataAccordion';
+export { default as DataAccordion } from './components/DataAccordion';
+export type { RepeaterCarouselProps } from './components/DataCarousel';
+export { default as RepeaterCarousel } from './components/DataCarousel';
+export type { RepeaterProps } from './components/Repeater';
+export { default as Repeater } from './components/Repeater';
+export type { RepeaterTableProps } from './components/RepeaterTable';
+export { default as RepeaterTable } from './components/RepeaterTable';
+export type { DataTableProps } from './components/Table';
+export { default as DataTable } from './components/Table';
+export type { DataTabsProps, TabItem } from './components/Tabs';
+export { default as DataTabs } from './components/Tabs';
