1a16fba50e552bd864155806406c79946d3c47f2
Simo Rudra <simo.rudra@gmail.com>
2026-07-27 18:48:30 +0000

    Publish library rudra-widgets version 1.0.6

--- libs/rudra-widgets/src/index.ts
+++ libs/rudra-widgets/src/index.ts
@@ -12,6 +12,8 @@ export type { RepeaterProps } from './components/Repeater';
 export { default as Repeater } from './components/Repeater';
 export type { RepeaterTableProps } from './components/RepeaterTable';
 export { default as RepeaterTable } from './components/RepeaterTable';
+export type { SeparatorProps } from './components/Separator';
+export { default as Separator } from './components/Separator';
 export type { DataTableProps } from './components/Table';
 export { default as DataTable } from './components/Table';
 export type { DataTabsProps, TabItem } from './components/Tabs';
